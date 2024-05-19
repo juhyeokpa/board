29f19577ad1d45009dd758e7491a9e17772f8327
juhyeoke <wngur3033@naver.com>
2024-05-19 15:44:03 +0900

    게시판

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("== 자바 텍스트 게시판==");
+        System.out.println("==프로그램 시작==");
+
+        while (true){
+            System.out.println("명령)");
+            String cmd = sc.nextLine();
+
+            if(cmd.equals("exit") ){
+                System.out.println("프로그램을 종료합니다");
+                break;
+            }
+            System.out.printf("입력된 명령어 : %s\n", cmd);
+        }
+
+        sc.close();
+    }
+}
